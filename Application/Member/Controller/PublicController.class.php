86cbdd81bf3cd5a901d659c643b3598d6598596b
adminlyy <13476121985@163.com>
2017-01-06 13:57:49 +0800

    git commit -"second"

--- Application/Member/Controller/PublicController.class.php
+++ Application/Member/Controller/PublicController.class.php
@@ -15,20 +15,52 @@ class PublicController extends BaseController{
 
     public function login(){
         if(IS_POST){
-            $username=trim($_POST['username']);
-            $pwd=trim($_POST['pwd']);
-            $data=M('user')->where("user_name='$username' and user_pwd='$pwd'")->select();
-            //dump($data);exit;
-            //$date=date('Y-m-d H:i:s');
-            if($data){
-
-                $this->success('登录成功',U('Course/index'),1);
-                exit();
-                //setcookie('username','pwd');
+            // 接受表单
+            $userModel = D('Public');
+/*            $data=I('post.');
+            dump($data);exit;*/
+            $data['user_name']=$_POST['user_name'];
+            $data['user_pwd']=$_POST['user_pwd'];
+            $data['code']=$_POST['code'];
+/*            $data1=$userModel->create($data);
+            dump($data);exit;*/
+            if($userModel->create($data,4)){
+                // 用户信息合法性检查
+                $status = $userModel->login();
+                if($status === true){
+                    $this->success('登录成功！', U('Course/index'));
+                    exit();
+                }else{
+                    $status == 1 ? $this->error('用户名不存在！') : $this->error('密码错误！');
+                }
             }else{
-                $this->error('用户名或密码错误','login',3);
+                $this->error('登录失败！'.$userModel->getError());
             }
         }
+
         $this->display();
+    }
+
+    // 用户退出
+    public function logout()
+    {
+        // 清除session数据
+        session(null);
+        // 清除cookie数据
+        setcookie('id','', time()-1, '/');
+        $this->success('退出成功！', U('Public/login'));
+        exit();
+    }
+
+    //验证码
+    public function code(){
+        $config = array(
+            'length' => 3,
+            'useNoise' => false,
+            'useCurve' => false,
+        );
+
+        $Verify = new \Think\Verify($config);
+        $Verify->entry();
     }
 }
