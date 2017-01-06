93f04deb900de5c363764e1ad8a1a1bf2d764176
adminlyy <13476121985@163.com>
2017-01-06 09:37:06 +0800

    git commit -"change public"

--- Application/Member/Controller/IndexController.class.php
+++ Application/Member/Controller/IndexController.class.php
@@ -16,7 +16,6 @@ class IndexController extends BaseController{
     public function index(){
 
 
-
         $this->display();
     }
 
--- Application/Member/Controller/PublicController.class.php
+++ Application/Member/Controller/PublicController.class.php
@@ -14,9 +14,21 @@ class PublicController extends BaseController{
 
 
     public function login(){
+        if(IS_POST){
+            $username=trim($_POST['username']);
+            $pwd=trim($_POST['pwd']);
+            $data=M('user')->where("user_name='$username' and user_pwd='$pwd'")->select();
+            //dump($data);exit;
+            //$date=date('Y-m-d H:i:s');
+            if($data){
 
+                $this->success('登录成功',U('Course/index'),1);
 
-
+                //setcookie('username','pwd');
+            }else{
+                $this->error('用户名或密码错误','login',3);
+            }
+        }
         $this->display();
     }
 }
